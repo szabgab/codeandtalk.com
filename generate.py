a4d7baeb11beb33186e3981965a433df1fec3618
Gabor Szabo <gabor@szabgab.com>
2016-11-10 14:58:09 +0200

    move the read function to the module

--- generate.py
+++ generate.py
@@ -7,125 +7,16 @@ import sys
 import re
 import shutil
 import urllib
-import csv
 from jinja2 import Environment, PackageLoader
 
-from xcast.people import read_people
+from xcast.people import read_people, read_tags, read_videos, read_events, read_episodes
 
 if sys.version_info.major < 3:
     exit("This code requires Python 3.\nThis is {}".format(sys.version))
 
-def read_tags():
-    tags = {}
-    with open('data/tags.csv', encoding="utf-8") as fh:
-        rd = csv.DictReader(fh, delimiter=';') 
-        for row in rd:
-            row['path'] = topic2path(row['tag'])
-            row['episodes'] = []
-            tags[ row['path'] ] = row
-    return tags
-
-def read_videos(topics):
-    root = 'data/videos'
-    events = os.listdir(root)
-    videos = []
-    for event in events:
-        path = os.path.join(root, event)
-        for video_file in os.listdir(path):
-            video_file_path = os.path.join(path, video_file)
-            with open(video_file_path) as fh:
-                video = json.load(fh)
-                video['filename'] = video_file[0:-5]
-                video['event']    = event
-                #print(event)
-                #exit()
-                #print(video)
-                video['file_date'] = datetime.fromtimestamp( os.path.getctime(video_file_path) )
-                videos.append(video)
-
-                if 'tags' in video:
-                    tags = []
-                    for t in video['tags']:
-                        p = topic2path(t)
-                        tags.append({
-                            'text': t,
-                            'link': p,
-                        }) 
-                        if p not in topics:
-                            topics[p] = {
-                                'name' : t,
-                                'events' : [],
-                                'videos' : [],
-                            }
-                        topics[p]['videos'].append(video)
-                    video['tags'] = tags
-
-    return videos
-
-
-def read_files():
-    conferences = []
-    topics = {}
-    now = datetime.now().strftime('%Y-%m-%d')
-
-    for filename in glob.glob("data/events/*.txt"):
-        print("Reading {}".format(filename))
-        conf = {}
-        try:
-            this = {}
-            nickname = os.path.basename(filename)
-            nickname = nickname[0:-4]
-            #print(nickname)
-            this['nickname'] = nickname
-            this['file_date'] = datetime.fromtimestamp( os.path.getctime(filename) )
-            with open(filename, encoding="utf-8") as fh:
-                for line in fh:
-                    line = line.rstrip('\n')
-                    if re.search(r'\A\s*#', line):
-                        continue
-                    if re.search(r'\A\s*\Z', line):
-                        continue
-                    line = re.sub(r'\s+\Z', '', line)
-                    k,v = re.split(r'\s*:\s*', line, maxsplit=1)
-                    this[k] = v
-
-            my_topics = []
-            if this['topics']:
-                for t in re.split(r'\s*,\s*', this['topics']):
-                    p = topic2path(t)
-                    #if p == '':
-                    #    exit("ERROR {}".format(this))
-                    my_topics.append({
-                        'name' : t,
-                        'path' : p,
-                    })
-                    if p not in topics:
-                        topics[p] = {
-                            'name': t,
-                            'events' : [],
-                            'videos' : [],
-                        }
-                    topics[p]['events'].append(this)
-            this['topics'] = my_topics
-
-            this['cfp_class'] = 'cfp_none'
-            cfp = this.get('cfp_date', '')
-            if cfp != '':
-                if cfp < now:
-                    this['cfp_class'] = 'cfp_past'
-                else:
-                    this['cfp_class'] = 'cfp_future'
-
-            conferences.append(this)
-        except Exception as e:
-            exit("ERROR: {} in file {}".format(e, filename))
-
-    return sorted(conferences, key=lambda x: x['start_date']), topics
-
-
 
 def process_conferences():
-    conferences, topics = read_files()
+    conferences, topics = read_events()
     #print(conferences)
     people = read_people('data/people')
     videos = read_videos(topics) # bad bad that topics will be updated!
@@ -405,21 +296,10 @@ def preprocess_events(now, conferences, videos):
             }
         countries[country_page]['events'].append(event)
 
-        if 'city' not in event or not event['city']:
-            exit("City is missing from {}".format(event))
-
-        city_name = '{}, {}'.format(event['city'], event['country'])
-        city_page = topic2path('{} {}'.format(event['city'], event['country']))
-
-        # In some countris we require state:
-        if event['country'] in ['Australia', 'Brasil', 'India', 'USA']:
-            if 'state' not in event or not event['state']:
-                exit('State is missing from {}'.format(event))
-            city_name = '{}, {}, {}'.format(event['city'], event['state'], event['country'])
-            city_page = topic2path('{} {} {}'.format(event['city'], event['state'], event['country']))
+        city_page = event['city_page']
         if city_page not in cities:
             cities[city_page] = {
-                'name' : city_name,
+                'name' : event['city_name'],
                 'events' : []
             }
         cities[city_page]['events'].append(event)
@@ -471,14 +351,6 @@ def html2txt(html):
     #text = re.sub(r'<a\s+href="[^"]+">([^<]+)</a>', '$1', html)
     text = re.sub(r'</?[^>]+>', '', html)
     return text
-
-def topic2path(tag):
-    t = tag.lower()
-    t = re.sub(r'í', 'i', t)
-    t = re.sub(r'ó', 'o', t)
-    t = re.sub(r'ã', 'a', t)
-    t = re.sub(r'[\W_]+', '-', t)
-    return t
 
 def main():
 
@@ -579,11 +451,11 @@ def main():
             #exit()
             if 'tags' in e:
                 for tag in e['tags']:
-                    path = topic2path(tag)
+                    path = tag['link']
                     if path not in tags:
                         # TODO report tag missing from the tags.csv file
                         tags[path] = {}
-                        tags[path]['tag'] = tag
+                        tags[path]['tag'] = tag['text']
                         tags[path]['episodes'] = []
                     tags[path]['episodes'].append(e)
 
@@ -709,29 +581,6 @@ def generate_podcast_pages(sources, people, tags, episodes):
     with open('html/search.json', 'w', encoding="utf-8") as fh:
         json.dump(search, fh)
 
-
-def read_episodes(sources):
-    episodes = []
-    for src in sources:
-        print("Processing source {}".format(src['name']))
-        file = 'data/podcasts/' + src['name'] + '.json'
-        src['episodes'] = []
-        if os.path.exists(file):
-            with open(file, encoding="utf-8") as fh:
-                try:
-                    new_episodes = json.load(fh)
-                    for episode in new_episodes:
-                        episode['source'] = src['name']
-                        if 'ep' not in episode:
-                            #print("WARN ep missing from {} episode {}".format(src['name'], episode['permalink']))
-                            pass
-                    episodes.extend(new_episodes)
-                    src['episodes'] = new_episodes
-                except json.decoder.JSONDecodeError as e:
-                    exit("ERROR: Could not read in {} {}".format(file, e))
-                    src['episodes'] = [] # let the rest of the code work
-                    pass
-    return episodes
 
 
 main()
--- xcast/people.py
+++ xcast/people.py
@@ -1,4 +1,7 @@
+import csv
+from datetime import datetime
 import glob
+import json
 import os
 import re
 
@@ -31,6 +34,178 @@ def read_people(path):
             exit("ERROR: {} in file {}".format(e, filename))
 
     return people
+
+def read_tags():
+    tags = {}
+    with open('data/tags.csv', encoding="utf-8") as fh:
+        rd = csv.DictReader(fh, delimiter=';') 
+        for row in rd:
+            row['path'] = topic2path(row['tag'])
+            row['episodes'] = []
+            tags[ row['path'] ] = row
+    return tags
+
+def read_videos(topics):
+    root = 'data/videos'
+    events = os.listdir(root)
+    videos = []
+    for event in events:
+        path = os.path.join(root, event)
+        for video_file in os.listdir(path):
+            video_file_path = os.path.join(path, video_file)
+            with open(video_file_path) as fh:
+                video = json.load(fh)
+                video['filename'] = video_file[0:-5]
+                video['event']    = event
+                #print(event)
+                #exit()
+                #print(video)
+                video['file_date'] = datetime.fromtimestamp( os.path.getctime(video_file_path) )
+                videos.append(video)
+
+                if 'tags' in video:
+                    tags = []
+                    for t in video['tags']:
+                        p = topic2path(t)
+                        tags.append({
+                            'text': t,
+                            'link': p,
+                        }) 
+                        if p not in topics:
+                            topics[p] = {
+                                'name' : t,
+                                'events' : [],
+                                'videos' : [],
+                            }
+                        topics[p]['videos'].append(video)
+                    video['tags'] = tags
+
+    return videos
+
+def read_events():
+    conferences = []
+    topics = {}
+    now = datetime.now().strftime('%Y-%m-%d')
+
+    for filename in glob.glob("data/events/*.txt"):
+        print("Reading {}".format(filename))
+        conf = {}
+        try:
+            this = {}
+            nickname = os.path.basename(filename)
+            nickname = nickname[0:-4]
+            #print(nickname)
+            this['nickname'] = nickname
+            this['file_date'] = datetime.fromtimestamp( os.path.getctime(filename) )
+            with open(filename, encoding="utf-8") as fh:
+                for line in fh:
+                    line = line.rstrip('\n')
+                    if re.search(r'\A\s*#', line):
+                        continue
+                    if re.search(r'\A\s*\Z', line):
+                        continue
+                    line = re.sub(r'\s+\Z', '', line)
+                    k,v = re.split(r'\s*:\s*', line, maxsplit=1)
+                    this[k] = v
+
+            my_topics = []
+            if this['topics']:
+                for t in re.split(r'\s*,\s*', this['topics']):
+                    p = topic2path(t)
+                    #if p == '':
+                    #    exit("ERROR {}".format(this))
+                    my_topics.append({
+                        'name' : t,
+                        'path' : p,
+                    })
+                    if p not in topics:
+                        topics[p] = {
+                            'name': t,
+                            'events' : [],
+                            'videos' : [],
+                        }
+                    topics[p]['events'].append(this)
+            this['topics'] = my_topics
+
+            this['cfp_class'] = 'cfp_none'
+            cfp = this.get('cfp_date', '')
+            if cfp != '':
+                if cfp < now:
+                    this['cfp_class'] = 'cfp_past'
+                else:
+                    this['cfp_class'] = 'cfp_future'
+
+            if 'city' not in this or not this['city']:
+                exit("City is missing from {}".format(this))
+
+            city_name = '{}, {}'.format(this['city'], this['country'])
+            city_page = topic2path('{} {}'.format(this['city'], this['country']))
+
+            # In some countris we require state:
+            if this['country'] in ['Australia', 'Brasil', 'India', 'USA']:
+                if 'state' not in this or not this['state']:
+                    exit('State is missing from {}'.format(this))
+                city_name = '{}, {}, {}'.format(this['city'], this['state'], this['country'])
+                city_page = topic2path('{} {} {}'.format(this['city'], this['state'], this['country']))
+            this['city_name'] = city_name
+            this['city_page'] = city_page
+
+            conferences.append(this)
+        except Exception as e:
+            exit("ERROR: {} in file {}".format(e, filename))
+
+    return sorted(conferences, key=lambda x: x['start_date']), topics
+
+def read_episodes(sources):
+    episodes = []
+    for src in sources:
+        print("Processing source {}".format(src['name']))
+        file = 'data/podcasts/' + src['name'] + '.json'
+        src['episodes'] = []
+        if os.path.exists(file):
+            with open(file, encoding="utf-8") as fh:
+                try:
+                    new_episodes = json.load(fh)
+                    for episode in new_episodes:
+                        episode['source'] = src['name']
+                        if 'ep' not in episode:
+                            #print("WARN ep missing from {} episode {}".format(src['name'], episode['permalink']))
+                            pass
+                    episodes.extend(new_episodes)
+                    src['episodes'] = new_episodes
+                except json.decoder.JSONDecodeError as e:
+                    exit("ERROR: Could not read in {} {}".format(file, e))
+                    src['episodes'] = [] # let the rest of the code work
+                    pass
+
+    for e in episodes:
+        #print(e)
+        #exit()
+        if 'tags' in e:
+            tags = []
+            for tag in e['tags']:
+                path = topic2path(tag)
+                if path not in tags:
+                    # TODO report tag missing from the tags.csv file
+                    tags.append({
+                        'text' : tag,
+                        'link' : path,
+                    })
+            e['tags'] = tags
+
+
+    return episodes
+
+
+def topic2path(tag):
+    t = tag.lower()
+    t = re.sub(r'í', 'i', t)
+    t = re.sub(r'ó', 'o', t)
+    t = re.sub(r'ã', 'a', t)
+    t = re.sub(r'[\W_]+', '-', t)
+    return t
+
+
 
 # vim: expandtab
 
