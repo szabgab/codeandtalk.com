201d8a76b83c299fb2cee057561fa771d6e82977
Gabor Szabo <gabor@szabgab.com>
2016-11-24 20:26:21 +0200

    list only people who have at least one something

--- bin/people.py
+++ bin/people.py
@@ -8,12 +8,19 @@ from cat.code import GenerateSite
 # list people who only have their name
 
 gs = GenerateSite()
-
-gs.read_people()
+gs.generate_site()  #blush
 for nickname in gs.people:
     fields = gs.people[nickname]['info'].keys()
     if len(fields) == 1:
-        print("{}  https://codeandtalk.com/p/{}".format(gs.people[nickname]['info']['name'], nickname))
+        videos = len(gs.people[nickname]['videos'])
+        episodes = len(gs.people[nickname]['episodes'])
+        hosting = len(gs.people[nickname]['hosting'])
+        total = videos + episodes + hosting
+        # for now let's only care about people who have at least one something in the database
+        # later, when we merge the videos from PerlTV more people will have something
+        if total > 0:
+            print("{}  https://codeandtalk.com/p/{}".format(gs.people[nickname]['info']['name'], nickname))
+        #print(gs.people[nickname])
 
 # vim: expandtab
 
