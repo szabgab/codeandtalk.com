cb1a11cba34cdd6eb66beca9742cf6dfc8ef784e
Gabor Szabo <gabor@szabgab.com>
2016-12-10 10:26:44 +0200

    code now reformats all the video files

--- bin/clean.py
+++ bin/clean.py
@@ -5,16 +5,17 @@ import glob
 # format the json files
 
 for filename in glob.glob("data/*.json") + glob.glob("data/podcasts/*.json"):
-	with open(filename) as fh:
-		data = json.load(fh)
-	with open(filename, 'w') as fh:
-		json.dump(data, fh, sort_keys=True, indent=4, separators=(',', ': '))
+    with open(filename) as fh:
+        data = json.load(fh)
+    with open(filename, 'w') as fh:
+        json.dump(data, fh, sort_keys=True, indent=4, separators=(',', ': '))
 
 for filename in glob.glob("data/videos/*/*.json"):
-	with open(filename) as fh:
-		data = json.load(fh)
-	if data['speakers']:
-		#print(filename)
-		with open(filename, 'w') as fh:
-			json.dump(data, fh, sort_keys=True, indent=4, separators=(',', ': '))
+    with open(filename) as fh:
+        data = json.load(fh)
+    #if data['speakers']:
+        #print(filename)
+    with open(filename, 'w') as fh:
+        json.dump(data, fh, sort_keys=True, indent=4, separators=(',', ': '))
 
+# vim: expandtab
