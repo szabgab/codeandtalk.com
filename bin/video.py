cac5f023cb888c6f61e0665cd71cde00ed07a485
Gabor Szabo <gabor@szabgab.com>
2016-12-15 17:27:26 +0200

    report vimeos separately

--- bin/video.py
+++ bin/video.py
@@ -15,12 +15,14 @@ gs.read_tags()
 gs.read_events()
 no_videos = ''
 no_youtube = ''
+on_vimeo = ''
 for e in sorted(gs.conferences, key=lambda e: e['start_date'], reverse=True):
     #exit(e)
     if e.get('videos_url'):
         continue
 
     youtube = e.get('youtube')
+    vimeo = e.get('vimeo')
 
     if e['end_date'] > gs.now:
         if youtube:
@@ -31,12 +33,18 @@ for e in sorted(gs.conferences, key=lambda e: e['start_date'], reverse=True):
         if youtube != '-':
             if not os.path.exists('data/videos/' + e['nickname']):
                 no_videos += "--list {:30} -d {} -e {}\n".format( youtube, e['start_date'], e['nickname'])
+    elif vimeo:
+        on_vimeo += "vimeo {} {}\n".format( e['start_date'], e['nickname'] )
     else:
         no_youtube += "{} {}\n".format( e['start_date'], e['nickname'] )
 
 if no_videos:
     print("Has youtube ID but videos were not included")
     print(no_videos)
+
+if on_vimeo:
+    print("On vimeo")
+    print(on_vimeo)
 
 if no_youtube:
     print("Has no youtube ID")
