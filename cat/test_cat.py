b148361687233a52646589dc8e9a745e95764729
Gabor Szabo <gabor@szabgab.com>
2017-01-14 09:27:06 +0200

    generate tests are not relevant any more

--- cat/test_cat.py
+++ cat/test_cat.py
@@ -28,33 +28,33 @@ class TestCat(unittest.TestCase):
         assert b'Oh. There is no page here.' in rv.data
 
 
-class TestDemo(unittest.TestCase):
-    def test_generate(self):
-        GenerateSite().generate_site()
-        assert True
-
-        # This fails on travis, we probably need better reporting to see what is the actual difference
-        # as I cannot see it. Unless it is only the file_date
-        files = [
-            'html/v/yougottalovefrontend-2016/vitaly-friedman-cutting-edge-responsive-web-design.json',
-            'html/p/zohar-babin.json',
-        ]
-        for result_file in files:
-            expected_file = 'samples/' + os.path.basename(result_file)
-            #sys.stderr.write(result_file)
-            #sys.stderr.write("\n")
-            #sys.stderr.write(expected_file)
-            #sys.stderr.write("\n")
-            # read both files
-            result = read_json(result_file)
-            expected = read_json(expected_file)
-            if 'file_date' in expected:
-                del(expected['file_date'])
-                del(result['file_date'])
-            if result != expected:
-                print("While testing {}\n".format(result_file))
-                print("Expected: {}".format(expected))
-                print("Received: {}".format(result))
-            assert result == expected
+#class TestDemo(unittest.TestCase):
+#    def test_generate(self):
+#        GenerateSite().generate_site()
+#        assert True
+#
+#        # This fails on travis, we probably need better reporting to see what is the actual difference
+#        # as I cannot see it. Unless it is only the file_date
+#        files = [
+#            'html/v/yougottalovefrontend-2016/vitaly-friedman-cutting-edge-responsive-web-design.json',
+#            'html/p/zohar-babin.json',
+#        ]
+#        for result_file in files:
+#            expected_file = 'samples/' + os.path.basename(result_file)
+#            #sys.stderr.write(result_file)
+#            #sys.stderr.write("\n")
+#            #sys.stderr.write(expected_file)
+#            #sys.stderr.write("\n")
+#            # read both files
+#            result = read_json(result_file)
+#            expected = read_json(expected_file)
+#            if 'file_date' in expected:
+#                del(expected['file_date'])
+#                del(result['file_date'])
+#            if result != expected:
+#                print("While testing {}\n".format(result_file))
+#                print("Expected: {}".format(expected))
+#                print("Received: {}".format(result))
+#            assert result == expected
 
 # vim: expandtab
