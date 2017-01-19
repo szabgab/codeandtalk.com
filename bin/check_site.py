f6bb2cbe4b298368ddcb032d75d6189d230e192f
Gabor Szabo <gabor@szabgab.com>
2017-01-19 07:05:32 +0200

    checking the live site

--- bin/check_site.py
+++ bin/check_site.py
@@ -4,17 +4,51 @@ import requests
 urls = [
 	'https://codeandtalk.com/',
 	'https://codeandtalk.com/conferences',
+	'https://codeandtalk.com/countries',
+	'https://codeandtalk.com/cities',
+	'https://codeandtalk.com/code-of-conduct',
+	'https://codeandtalk.com/diversity-tickets',
+	'https://codeandtalk.com/series',
+	'https://codeandtalk.com/all-conferences',
+
 	'https://codeandtalk.com/topics',
+	'https://codeandtalk.com/t/angularjs',
+
+	'https://codeandtalk.com/podcasts',
+	'https://codeandtalk.com/s/changelog',
+
+	'https://codeandtalk.com/people',
+	'https://codeandtalk.com/people?term=sz',
+
+	'https://codeandtalk.com/cal/all.ics', # calendar
 	
 	'https://codeandtalk.com/e/wootconf-2017',
 	
+	'https://codeandtalk.com/videos',
+	'https://codeandtalk.com/videos?term=Hebrew&mindate=&maxdate=&mintime=&maxtime=',
 	'https://codeandtalk.com/v/react-conf-2016/reactjs-conf-2016-lightning-talks-vivian-cromwell',
+	'https://codeandtalk.com/v/devoxx-france-2016/10-enseignements-quon-peut-tirer-des-31463-commits-qui-ont-cree-le-langage-simone-civetta', # Has Unicode error
+
+	'https://codeandtalk.com/about',
+
 	'https://codeandtalk.com/sitemap.xml',
 ]
 
+failures = []
 for url in urls:
 	response = requests.get(url)	
-	if response.status_code != 200:
-		print("failed {}".format(url))
-		print(response.status_code)
+	if response.status_code == 200:
+		print('    OK     {}'.format(url))
+	else:
+		failures.append({
+			'url'  : url,
+			'code' : response.status_code,
+		})
+		print("NOT OK {:3} {}".format(response.status_code, url))
+print("-------------")
+if failures:
+	print("ERROR")
+	print(failures)
+else:
+	print("SUCCESS")
 
