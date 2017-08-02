a0ef3b25718f6a16d663f8954284b1440bf6bd9b
Gabor Szabo <gabor@szabgab.com>
2017-08-02 07:29:45 +0300

    save strings as readable characters instead of the \u representation

--- bin/tidy_json.py
+++ bin/tidy_json.py
@@ -8,7 +8,7 @@ def tidy(filename):
     with open(filename) as fh:
         data = json.load(fh)
     with open(filename, 'w') as fh:
-        json.dump(data, fh, sort_keys=True, indent=4, separators=(',', ': '))
+        json.dump(data, fh, sort_keys=True, indent=4, separators=(',', ': '), ensure_ascii=False)
 
 for filename in glob.glob("data/*.json"):
     tidy(filename)
