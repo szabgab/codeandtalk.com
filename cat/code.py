56bd28f755d72f3092a589ef2c8745a4497d446c
Gabor Szabo <gabor@szabgab.com>
2017-10-23 15:14:23 +0300

    India locations

--- cat/code.py
+++ cat/code.py
@@ -269,7 +269,7 @@ class GenerateSite(object):
             city_name = '{}, {}, {}'.format(location['city'], location['state'], location['country'])
             city_page = topic2path('{} {} {}'.format(location['city'], location['state'], location['country']))
 
-            if location['country'] in ['Australia', 'Brasil']:
+            if location['country'] in ['Australia', 'Brasil', 'India']:
                 if location['state'] not in self.locations[ location['country'] ]:
                     raise Exception("Invalid state '{}' in {}".format(location['state'], this))
                 if location['city'] not in self.locations[ location['country'] ][ location['state'] ]:
