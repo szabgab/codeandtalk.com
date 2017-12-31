863291a23ffe25cbcadcb44ddf3e5e32004f33ee
Paul Jacobson <pljcbsn@gmail.com>
2017-12-31 19:44:52 +0200

    Moved future function to tools so we can reuse from notify

--- cat/tools.py
+++ cat/tools.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 
 def in_sec(length):
     parts = [int(x) for x in length.split(':')]
@@ -7,5 +8,11 @@ def in_sec(length):
         sec *= 60
         sec += parts.pop(0)
     return sec
+
+def future(cat):
+    now = datetime.now().strftime('%Y-%m-%d')
+    return sorted(
+        list(filter(lambda e: e['event_start'] >= now, cat["events"].values())),
+        key = lambda e: e['event_start'])
 
 # vim: expandtab
