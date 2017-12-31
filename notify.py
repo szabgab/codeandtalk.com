828530e21d146a4e24106d8d6eea394dc8b297a4
leora-betesh <leorab@suite-sol.com>
2017-12-31 20:57:28 +0200

    moved notify.py to the root

---
+++ notify.py
@@ -0,0 +1,26 @@
+import json
+import os
+import sys
+from jinja2 import Environment, PackageLoader
+
+env =  Environment(loader=PackageLoader('cat'))
+template = env.get_template('email.html')
+
+root = os.path.dirname((os.path.realpath(__file__)))
+sys.path.insert(0, root)
+from cat import tools
+from cat.tools import read_json
+
+with open('subscribers.json', 'r') as fh:
+  subscriptions = json.load(fh)
+
+print(subscriptions)
+
+cat = read_json(root + '/html/cat.json')
+conferences = tools.future(cat)
+
+print(template.render(
+  events = conferences, title="Hello"
+))
+
+
